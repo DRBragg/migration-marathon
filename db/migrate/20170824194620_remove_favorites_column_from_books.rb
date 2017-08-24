a07acf74fbaae23885dc027f08f4d36da938283e
Drew Bragg <DRBragg@gmail.com>
2017-08-24 15:47:11 -0400

    Step 5

---
+++ db/migrate/20170824194620_remove_favorites_column_from_books.rb
@@ -0,0 +1,5 @@
+class RemoveFavoritesColumnFromBooks < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :books, :favorite
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824194304) do
+ActiveRecord::Schema.define(version: 20170824194620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20170824194304) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author", null: false
-    t.boolean "favorite", default: false
     t.integer "rating"
   end
 
