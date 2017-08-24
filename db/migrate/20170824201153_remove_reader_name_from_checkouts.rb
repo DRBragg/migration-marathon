3d2bb3dc207c599e33da5fdcc9f21436e9e7b4f1
Drew Bragg <DRBragg@gmail.com>
2017-08-24 16:14:08 -0400

    Step 11, step does nothing due to incomplete instructions

---
+++ db/migrate/20170824201153_remove_reader_name_from_checkouts.rb
@@ -0,0 +1,5 @@
+class RemoveReaderNameFromCheckouts < ActiveRecord::Migration[5.1]
+  def change
+    #This migration will not do anything, the setps in the challenge are unclear and a reader name was not originally added to out checkouts table if it was this migration would remove that column.
+  end
+end
